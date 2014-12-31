f46a7d85776f8131e3e013518f7f590c0e43475d
Brian Ball <btball@gmail.com>
2014-12-31 14:00:21 -0800

    Added compass rose and adjustable font.

--- Adv550/version.h
+++ Adv550/version.h
@@ -2,12 +2,11 @@
 //  version.h
 //  Adv550
 //
-//  Created by Brian Ball on 12/29/14.
+//  Created by Brian Ball on 12/31/14.
 //  Copyright (c) 2014 Brian. All rights reserved.
 //
 
 #ifndef Adv550_version_h
-#define Adv550_version_h
-char * adv_version = "_START_HTML_adv550";
-
+#define Adv550_version_
 #endif
+char * adv_version = "_START_HTML_adv550";
